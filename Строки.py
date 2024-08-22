ccc342ed7af564575e7bb5b1c0e016df10a5453d
Katrin <Katinka.Ilicheva@yandex.ru>
2024-08-22 13:22:05 -0700

    Update Строки.py

--- Строки.py
+++ Строки.py
@@ -1,6 +1,7 @@
 example = "строчки"
 print(example[0]) #первый символ строки
 print(example[-1]) #последний символ строки
-print(example[4:7]) #вторая половина строки
+a = (len(example) + 1) #вторая половина строки
+print(example[a // 2:])
 print(example[::-1]) #слово наоборот
 print(example[1:7:2]) #каждый второй символ строки
